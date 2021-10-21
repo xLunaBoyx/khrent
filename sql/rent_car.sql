e75992c4cd86470c42a60ff03e4085dd1dd149f6
dlcksdud <cksdud5087@naver.com>
2021-10-21 17:34:18 +0900

    rent_car.sql 변경
    member테이블 생성, 홍길동회원 삽입

---
+++ sql/rent_car.sql
@@ -0,0 +1,24 @@
+-- semi 계성 생성함
+
+CREATE TABLE member (
+	member_id	varchar2(20)		NOT NULL,
+	member_pwd	varchar2(300)		NOT NULL,
+	member_role	char(1)	DEFAULT 'U' 	NOT NULL,
+	member_name	 varchar2(50)		NOT NULL,
+	phone	char(11)		NOT NULL,
+	mileage 	number	DEFAULT 0	NOT NULL,
+	reg_date	date	DEFAULT sysdate	NOT NULL
+);
+
+ALTER TABLE member ADD CONSTRAINT "PK_MEMBER" PRIMARY KEY (
+	member_id
+);
+alter table member add constraint ck_member_role check (member_role in ('U', 'A')); 
+
+select * from member;
+
+insert into member
+values (
+    'honggd', '1234', 'U', '홍길동',
+    '01012341234', 1000,  default
+);
