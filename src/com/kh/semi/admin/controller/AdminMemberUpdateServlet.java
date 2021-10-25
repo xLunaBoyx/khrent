35206ac7b6a184c9f90ed391afc8294a1c3f8825
user <user@DESKTOP-4UQ1C7A>
2021-10-25 18:31:08 +0900

    회원 정보 변경 수정2

--- src/com/kh/semi/admin/controller/AdminMemberUpdateServlet.java
+++ src/com/kh/semi/admin/controller/AdminMemberUpdateServlet.java
@@ -45,7 +45,41 @@ public class AdminMemberUpdateServlet extends HttpServlet {
 	 */
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 1. encoding 처리
+		request.setCharacterEncoding("UTF-8");
+		
+		// 2. 사용자입력값 -> Member객체
+		String memberId = request.getParameter("memberId");
+		String password = request.getParameter("password");
+		String memberName = request.getParameter("memberName");
+		String phone = request.getParameter("phone");
+		String license_type = request.getParameter("license_type");
+		String license_no = request.getParameter("license_no");
+		String issue_date = request.getParameter("issue_date");
+	
+		Member member = new Member(memberId, null, memberName, MemberService.MEMBER_ROLE, phone, 0, null, license_type, license_no, issue_date);
 
+		System.out.println("member@servlet = " + member);
 
+		
+		// 3.업무로직 
+		int result = memberService.adminMemberUpdate(member);
+		String msg = result > 0 ? "회원정보 수정 성공" : "회원정보 수정 실패";
+		
+		// 성공적으로 회원정보를 수정했다면, session의 속성 loginMember객체도 갱신해야 한다.
+		if(result > 0) {
+			HttpSession session = request.getSession();
+			Member newMember = memberService.selectOneMember(memberId);
+			session.setAttribute("loginMember", newMember);
+		}
+		
+		// 4.응답처리 msg속성 저장후 /mvc로 redirect할 것.
+
+		HttpSession session = request.getSession();
+		session.setAttribute("msg", msg);
+		
+		String location = request.getContextPath() + "/";
+		response.sendRedirect(location);
 	}
 }
